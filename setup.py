f90760c505765f251b28afe8f9e0f86cfa49331f
Alexander Glyzov <bonoba@gmail.com>
2012-08-01 00:43:43 +0300

    initial commit (version 0.1)

---
+++ setup.py
@@ -0,0 +1,16 @@
+# vim: fileencoding=utf-8 et ts=4 sts=4 sw=4 tw=0 fdm=marker fmr=#{,#}
+
+from distutils.core import setup
+
+if __name__ == '__main__':
+    setup(
+        name         = 'zone53',
+        version      = '0.1',
+        description  = 'A convenient Python interface to AWS route53 on top of Boto',
+        author       = 'Alexander Glyzov',
+        author_email = 'bonoba@gmail.com',
+        url          = 'https://github.com/aglyzov/zone53',
+        packages     = ['zone53'],
+        requires     = ['boto (>= 2.0)']
+    )
+
